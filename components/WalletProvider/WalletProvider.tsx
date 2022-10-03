0106c6e954179b96c1470fd854165abc7d8880d0
ellagoris <eduardofidera@gmail.com>
2022-10-03 20:18:35 -0300

    feat: update deps and wallet provider

--- components/WalletProvider/WalletProvider.tsx
+++ components/WalletProvider/WalletProvider.tsx
@@ -1,17 +1,16 @@
-import React, { useMemo } from "react"
+import React, { FC, useMemo } from "react"
 import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react"
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base"
 import {
+  BackpackWalletAdapter,
   LedgerWalletAdapter,
   PhantomWalletAdapter,
   SlopeWalletAdapter,
-  SolflareWalletAdapter,
   SolletExtensionWalletAdapter,
   SolletWalletAdapter,
-  TorusWalletAdapter,
 } from "@solana/wallet-adapter-wallets"
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui"
 
@@ -20,6 +19,7 @@ require("@solana/wallet-adapter-react-ui/styles.css")
 
 const Wallet = ({ children }: { children: React.ReactChild }) => {
   // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+
   const network = process.env
     .NEXT_PUBLIC_CONNECTION_NETWORK as WalletAdapterNetwork
 
@@ -28,35 +28,34 @@ const Wallet = ({ children }: { children: React.ReactChild }) => {
       ? process.env.NEXT_PUBLIC_SOLANA_RPC_HOST_DEVNET
       : process.env.NEXT_PUBLIC_SOLANA_RPC_HOST_MAINNET_BETA
 
-  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
-  // Only the wallets you configure here will be compiled into your application, and only the dependencies
-  // of wallets that your users connect to will be loaded.
   const wallets = useMemo(
     () => [
+      /**
+       * Select the wallets you wish to support, by instantiating wallet adapters here.
+       *
+       * Common adapters can be found in the npm package `@solana/wallet-adapter-wallets`.
+       * That package supports tree shaking and lazy loading -- only the wallets you import
+       * will be compiled into your application, and only the dependencies of wallets that
+       * your users connect to will be loaded.
+       */
       new PhantomWalletAdapter(),
+      new BackpackWalletAdapter(),
       new SlopeWalletAdapter(),
-      new SolflareWalletAdapter({ network }),
-      new TorusWalletAdapter(),
       new LedgerWalletAdapter(),
       new SolletWalletAdapter({ network }),
       new SolletExtensionWalletAdapter({ network }),
     ],
-    [network]
+    []
   )
 
   return (
     <ConnectionProvider
       endpoint={endpoint}
-      config={{
-        commitment: "confirmed",
-      }}
+      config={{ commitment: "confirmed" }}
     >
       <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>
-          {/* <WalletMultiButton />
-          <WalletDisconnectButton /> */}
-
-          {children}
+          {children}{" "}
           {/* Your app's components go here, nested within the context providers. */}
         </WalletModalProvider>
       </WalletProvider>
